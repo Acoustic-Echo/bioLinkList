6d62d5691706b796ffc4cba9a84e5c357a15835d
Acoustic-Echo <58014508+Acoustic-Echo@users.noreply.github.com>
2024-11-09 11:03:43 +0900

    オンラインフレンドリスト取得処理の追加

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,53 @@
 const GET_TOKEN = "https://vrchat.com/api/1/auth";
+const GET_FRIENDS = "https://vrchat.com/api/1/auth/user/friends?n=100";
 var token = "";
+var friendsList;
 //読み込み終了時リスナー
 window.addEventListener('load', () => getToken(), false);
 
 //トークン取得
 const getToken = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", GET_TOKEN);
-    xhr.send();
-    xhr.onload = () =>{
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            const DATA = xhr.response;
-            const JSON_DATA = JSON.parse(DATA);
-            token = JSON_DATA.token
-          } else {
-            console.log(`Error: ${xhr.status}`);
-          }
-    }
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", GET_TOKEN);
+  xhr.send();
+  xhr.onload = () =>{
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          const DATA = xhr.response;
+          const JSON_DATA = JSON.parse(DATA);
+          token = JSON_DATA.token
+        } else {
+          console.log(`Error: ${xhr.status}`);
+        }
+  }
+  getOnlineFriends();
 };
+
+const getOnlineFriends = () => {
+  var count = 100;
+  var url = GET_FRIENDS;
+  var jsonData;
+  fetchFriends(url).then(result => {
+    console.log(result);
+    friendsList = result;
+  })
+  .catch(error => {console.error(error)});
+};
+
+const fetchFriends = async (url) => {
+  var jsonData = [];
+  var elementNum = 100;
+  for(let i=0;i<5;i++){
+    if(elementNum >= 100){
+      const RESPONCE = await fetch(GET_FRIENDS);
+      const JSON_DATA = await RESPONCE.json();
+      elementNum = Object.keys(JSON_DATA).length;
+      console.log(elementNum);
+      jsonData.push(JSON_DATA);
+    }else{
+      break;
+    }
+  }
+  return jsonData;
+};
+
+
